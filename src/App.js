c2bb71b262003b75128830a41de17621dd0fac0e
Claude Abounegm <claude.abounegm@gmail.com>
2020-01-30 16:07:02 +0200

    basic nav bar functionality works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Nav } from "nav-tree";
 import NavBar from "./components/NavBar";
+import "semantic-ui-css/semantic.min.css";
 
 const nav = new Nav({}, nav => {
   nav.appendCategory({ title: "Category1" }, nav => {
     nav.appendLink({ title: "Link1", href: "/link1" });
-    nav.appendDivider();
+    // nav.appendDivider();
     nav.appendLink({ title: "Link2", href: "/link2" });
   });
 });
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,34 @@
 import React from "react";
+import { Menu, Dropdown } from "semantic-ui-react";
 
 const NavBar = props => {
   const { nav } = props;
 
   const els = nav.traverse((item, i, traverseChildren) => {
     if (item.type === "link") {
-      return <a href={item.href}>{item.title}</a>;
-    }
+      if (item.level === 0) {
+        return (
+          <Menu.Item name={item.id} href={item.href}>
+            {item.title}
+          </Menu.Item>
+        );
+      }
 
-    if (item.type === "divider") {
-      return <hr />;
+      return <Dropdown.Item href={item.href}>{item.title}</Dropdown.Item>;
     }
 
     if (item.type === "category") {
       return (
-        <div>
-          <h1>{item.title}</h1>
-          {traverseChildren()}
-        </div>
+        <Dropdown item text={item.title}>
+          <Dropdown.Menu>{traverseChildren()}</Dropdown.Menu>
+        </Dropdown>
       );
     }
 
-    return (
-      <div>
-        {item.type} - {item.title}
-      </div>
-    );
+    return null;
   });
 
-  return <div>{els}</div>;
+  return <Menu pointing>{els}</Menu>;
 };
 
 export default NavBar;
